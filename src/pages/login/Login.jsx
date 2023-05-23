2e8cf233a205ea6c50e4adb3d4a6bd5df3543edd
Nemi <belenemi.ms@gmail.com>
2023-05-22 21:19:29 -0400

    update (login): se incluyo header y footer

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,10 @@
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <Header /> <Footer />
+    </>
+  );
 }
