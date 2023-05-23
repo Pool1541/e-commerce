7cb111e5c6f3b13ae14a106ef54c4b27b3e04c44
Pool <pool_1541@hotmail.com>
2023-05-22 21:13:40 -0500

    update(Departments): loading state

--- src/pages/departments/Departments.jsx
+++ src/pages/departments/Departments.jsx
@@ -4,24 +4,29 @@ import { useEffect } from 'react';
 
 export default function Departments() {
   const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getProducts() {
       const response = await fetch('https://e-commerce-api-express.up.railway.app/api/products');
       const data = await response.json();
       setProducts(data);
+      setLoading(false);
     }
     getProducts();
   }, []);
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      {/* {products?.products?.map((product) => (
+      {products.products.map((product) => (
         <div key={product._id}>
-          <h1>{product.tittle}</h1>
+          <h1>{product.title}</h1>
         </div>
-      ))} */}
-      hola
+      ))}
     </div>
   );
 }
