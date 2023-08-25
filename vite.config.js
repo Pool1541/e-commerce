e9cbb4d5880a2440847971e6e7f619f7fb3edc18
Pool <pool_1541@hotmail.com>
2023-08-25 10:45:21 -0500

    chore(viteConfig): change base and outdir path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://pool1541.github.io/e-commerce',
-  build: {
-    outDir: 'build',
-  },
+  // base: 'https://pool1541.github.io/e-commerce',
+  // build: {
+  //   outDir: 'build',
+  // },
 });
