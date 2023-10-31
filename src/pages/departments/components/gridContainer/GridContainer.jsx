c4cb5188151a34c60842206268cb528a0d5f8d5c
Pool <pool_1541@hotmail.com>
2023-10-31 15:15:35 -0500

    refactor(departments): Improved code in the GridContainer component

--- src/pages/departments/components/gridContainer/GridContainer.jsx
+++ src/pages/departments/components/gridContainer/GridContainer.jsx
@@ -1,25 +1,18 @@
 import Card from '../card/Card';
 import useProduct from '../../../../hooks/useProducts';
-import { Grid } from './GridContainer.styled';
 import SkeletonCard from '../card/SkeletonCard';
+import { Grid } from './GridContainer.styled';
 
 export default function GridContainer() {
   const { products, loading } = useProduct();
 
-  if (loading)
-    return (
-      <Grid>
-        {Array.from({ length: 20 }).map((_, index) => (
-          <SkeletonCard key={index} />
-        ))}
-      </Grid>
-    );
+  const SkeletonCards = Array.from({ length: 20 }).map((_, index) => <SkeletonCard key={index} />);
 
   return (
     <Grid>
-      {products.products.map((product) => (
-        <Card key={product.id} product={product} />
-      ))}
+      {loading
+        ? SkeletonCards
+        : products.products.map((product) => <Card key={product.id} product={product} />)}
     </Grid>
   );
 }
