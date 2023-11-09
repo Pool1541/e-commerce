3a1ff25791b6f44b2ef3a7765ca1e9d9cb896c96
Pool <pool_1541@hotmail.com>
2023-11-08 22:54:54 -0500

    feat(sort): Added logic to toggle sorting options when clicking the sort button

--- src/pages/departments/components/buttons/SortButton.jsx
+++ src/pages/departments/components/buttons/SortButton.jsx
@@ -1,14 +1,21 @@
 import { SortIcon } from '../../../../assets/icons';
 import { Button } from '../../../../components/elements';
 import { StyledSortButtonContainer } from './Buttons.styled';
+import SortOptions from '../sortOptions/sortOptions';
+import useBoolean from '../../../../hooks/useBoolean';
+import useOutsideClick from '../../../../hooks/useOutsideClick';
 
 export default function SortButton() {
+  const { value, setToggle, setFalse } = useBoolean();
+  const ref = useOutsideClick(setFalse);
+
   return (
-    <StyledSortButtonContainer>
-      <Button>
+    <StyledSortButtonContainer ref={ref}>
+      <Button onClick={setToggle}>
         <SortIcon />
         <span>Ordenar</span>
       </Button>
+      {value && <SortOptions onClick={setFalse} />}
     </StyledSortButtonContainer>
   );
 }
