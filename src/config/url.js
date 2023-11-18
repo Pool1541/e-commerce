3f9287714ebdb51d777d0ab36824f8dd65d3bd65
Pool <pool_1541@hotmail.com>
2023-11-18 13:58:45 -0500

    chore(config): Updated API base URL

--- src/config/url.js
+++ src/config/url.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api'
-    : 'https://e-commerce-api-dev.4.us-1.fl0.io';
+    : 'https://e-commerce-api-dev.4.us-1.fl0.io/api';
