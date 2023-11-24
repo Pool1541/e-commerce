0a2f3039643f5089170aaa01d391a6e4382de317
pool_1541 <pool_1541@hotmail.com>
2023-11-24 00:30:07 -0500

    feat(guards): add unauthguard in router

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import AuthContextProvider from './context/AuthContext.jsx';
 import BasketContextProvider from './context/BasketContext.jsx';
 import Account from './pages/account/Account.jsx';
 import AuthGuard from './guards/AuthGuard.jsx';
+import UnAuthGuard from './guards/UnAuthGuard.jsx';
 
 function App() {
   return (
@@ -21,9 +22,11 @@ function App() {
           <Toaster richColors position='top-center' expand={true} closeButton />
           <Routes>
             <Route path={PUBLIC_ROUTES.HOME} element={<Home />} />
-            <Route path={PUBLIC_ROUTES.LOGIN} element={<Login />} />
             <Route path={`${PUBLIC_ROUTES.CATEGORY}/:categoryName`} element={<Departments />} />
             <Route path={`${PUBLIC_ROUTES.PRODUCT}/:productID`} element={<Product />} />
+            <Route element={<UnAuthGuard />}>
+              <Route path={PUBLIC_ROUTES.LOGIN} element={<Login />} />
+            </Route>
             <Route element={<AuthGuard />}>
               <Route path={`${PRIVATE_ROUTES.ACCOUNT}/*`} element={<Account />} />
             </Route>
