fda1d5fdaad3f2e79e614259bf7a4ff70246f24f
pool_1541 <pool_1541@hotmail.com>
2023-11-27 23:01:30 -0500

    chore(guards): Update href in UnAuthGuard

--- src/guards/UnAuthGuard.jsx
+++ src/guards/UnAuthGuard.jsx
@@ -1,9 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { ACCOUNT_ROUTES, PRIVATE_ROUTES } from '../config';
 import useAuth from '../hooks/useAuth';
-import { PRIVATE_ROUTES } from '../config';
 
 export default function UnAuthGuard() {
   const { isAuthenticated } = useAuth();
 
-  return isAuthenticated ? <Navigate replace to={PRIVATE_ROUTES.ACCOUNT} /> : <Outlet />;
+  return isAuthenticated ? (
+    <Navigate replace to={PRIVATE_ROUTES.ACCOUNT + '/' + ACCOUNT_ROUTES.PROFILE} />
+  ) : (
+    <Outlet />
+  );
 }
