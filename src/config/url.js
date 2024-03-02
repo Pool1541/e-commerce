81fb0a07cfd2c527062c31653a85eb046c13cfb7
pool_1541 <pool_1541@hotmail.com>
2024-03-01 22:57:03 -0500

    chore(config): update api url

--- src/config/url.js
+++ src/config/url.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api'
-    : 'https://e-commerce-api-dev.4.us-1.fl0.io/api';
+    : 'https://e-commerce-api-express.up.railway.app/api';
