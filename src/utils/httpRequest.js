321d4ef33bac783bba93b953f7a6f7c88e54a240
pool_1541 <pool_1541@hotmail.com>
2024-03-02 20:41:55 -0500

    chore(utils): improve httpRequest function

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -2,6 +2,7 @@ import { API_URL } from '../config';
 
 export async function httpRequest(options, endpoint) {
   const response = await fetch(`${API_URL}${endpoint}`, options);
+  if (response.status === 204) return;
   const data = await response.json();
 
   if (!response.ok) throw new Error(data.error);
