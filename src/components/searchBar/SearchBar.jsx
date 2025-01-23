43c62a4d795d20d96928892124df4ac783f2669a
pool_1541 <pool_1541@hotmail.com>
2025-01-23 15:25:45 -0500

    refactor(component): implement search functionality in SearchBar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,10 +1,22 @@
-import SearchIcon from '../../assets/icons/Search.icon';
-import { Button } from '../elements/Button.styled';
+import { useNavigate } from 'react-router-dom';
 import SearchBarContainer from './SearchBar.styled';
+import { Button } from '@/components/elements';
+import { SearchIcon } from '@/assets/icons';
 
 export default function SearchBar() {
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const keyword = e.target.querySelector('input').value.trim();
+
+    if (keyword) {
+      navigate(`/search?keyword=${keyword}`);
+    }
+  }
+
   return (
-    <SearchBarContainer>
+    <SearchBarContainer onSubmit={handleSubmit}>
       <input type='text' />
       <Button>Search</Button>
       <SearchIcon />
