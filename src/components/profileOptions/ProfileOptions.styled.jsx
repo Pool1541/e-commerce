348823b171e2aaa7d77a30894eaf99d3971b2f83
pool_1541 <pool_1541@hotmail.com>
2025-02-02 18:42:59 -0500

    style(component): update animation to improve visibility and scaling effect

--- src/components/profileOptions/ProfileOptions.styled.jsx
+++ src/components/profileOptions/ProfileOptions.styled.jsx
@@ -2,9 +2,11 @@ import styled, { keyframes } from 'styled-components';
 
 const show = keyframes`
   from {
-    transform: scale(0);
+    opacity: 0;
+    transform: scale(0.9);
   }
   to {
+    opacity: 1;
     transform: scale(1);
   }
 `;
