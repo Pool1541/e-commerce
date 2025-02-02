b34695a828f1b1d297b14a30d9cbcc49c9bde4d2
pool_1541 <pool_1541@hotmail.com>
2025-02-02 18:41:32 -0500

    refactor(component): make ChevronRightIcon component accept props for customization

--- src/assets/icons/ChevronRight.icon.jsx
+++ src/assets/icons/ChevronRight.icon.jsx
@@ -1,4 +1,4 @@
-export default function ChevronRightIcon() {
+export default function ChevronRightIcon(props) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -6,7 +6,8 @@ export default function ChevronRightIcon() {
       height='16'
       fill='currentColor'
       className='bi bi-chevron-right'
-      viewBox='0 0 16 16'>
+      viewBox='0 0 16 16'
+      {...props}>
       <path
         fillrrule='evenodd'
         d='M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z'
