e3773b04b26463c6f37cc3ab8e1e3ca5e1fc98bf
pool_1541 <pool_1541@hotmail.com>
2025-02-04 19:00:59 -0500

    refactor(repository): simplify getFilters function by consolidating query parameters

--- src/repositories/filterRepository.js
+++ src/repositories/filterRepository.js
@@ -1,14 +1,9 @@
 import { ENDPOINTS } from '@/config';
 import { httpRequest } from '@/utils';
 
-export async function getFilters({ category = '', keyword = '' }) {
+export async function getFilters({ query = '' }) {
   try {
-    const query = category ? `&category=${category}` : `&keyword=${keyword}`;
-
-    return await httpRequest(
-      {},
-      `${ENDPOINTS.GET_FILTERS_BY_NAME}?filters=subCategory,brand${query}`
-    );
+    return await httpRequest({}, `${ENDPOINTS.GET_FILTERS_BY_NAME}${query}`);
   } catch (error) {
     if (error.name === 'AbortError') {
       throw new DOMException('La solicitud ha sido cancelada.', 'AbortError');
